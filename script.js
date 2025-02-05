8a4c9232a6994cb734cd62b6af544a71b28e4b69
zaidalshaikh <zaidalshaiskh@gmail.com>
2025-02-05 12:56:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const links = [
     { name: "Facebook", url: "https://www.facebook.com/zaid.alshaiskh" },
     { name: "Whatsapp", url: "https://wa.me/962788850531" },
     { name: "Snapchat", url: "https://snapchat.com/t/6ObtCtIF" },
-     { name: "LinkedIn", url: "https://www.linkedin.com/in/zaidalshaikh", icon: "fa-brands fa-linkedin" }
+    { name: "LinkedIn", url: "https://www.linkedin.com/in/zaidalshaikh" }
 ];
 
 // Function to generate links dynamically
